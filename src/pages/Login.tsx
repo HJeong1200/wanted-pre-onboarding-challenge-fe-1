f90135c7e8ae325f347b033717a96ac0769b6640
HJeong1200 <hjeong1200@gmail.com>
2023-01-06 00:20:09 +0900

    [Feat] 이메일과 비밀번호 유효성 검증 기능

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,12 +1,57 @@
+import React, { useState } from "react";
+
 export default function LoginPage() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const emailValidation = (email: string) => {
+    if (email.includes("@") && email.includes(".")) return true;
+    return false;
+  };
+
+  const passwordValidation = (password: string) => {
+    if (password.length >= 8) return true;
+    return false;
+  };
+
+  const changeEmail = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const inputEmail = e.target.value;
+    setEmail(inputEmail);
+  };
+
+  const changePassword = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const inputPassword = e.target.value;
+    setPassword(inputPassword);
+  };
+
+  const submitLoginForm = () => {
+    if (emailValidation(email) && passwordValidation(password)) {
+      console.log(email, password);
+    } else {
+      console.log("Invalid");
+    }
+  };
+
   return (
     <div className="Login_Form_Container">
       <div className="Login_Title"></div>
       <label htmlFor="Email">Email</label>
-      <input type="text" id="Email" className="Login_Email" />
+      <input
+        type="text"
+        id="Email"
+        className="Login_Email"
+        onChange={changeEmail}
+      />
       <label htmlFor="Password">Password</label>
-      <input type="text" id="Password" className="Login_Password_Input" />
-      <button className="Login_Submit_Button">로그인</button>
+      <input
+        type="text"
+        id="Password"
+        className="Login_Password_Input"
+        onChange={changePassword}
+      />
+      <button className="Login_Submit_Button" onClick={submitLoginForm}>
+        로그인
+      </button>
     </div>
   );
 }
